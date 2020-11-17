01a1640f06a10e8cefb6287dc7a431b71050a999
Dariusz Parys <dparys@microsoft.com>
2020-11-17 19:46:19 +0100

    Adjusted Tests

--- test_mybackend.py
+++ test_mybackend.py
@@ -4,14 +4,14 @@ from modules.windowsfile import WindowsFile
 
 import testinfra
 
-def test_powershell_get_version():
-    command = "(get-host).version.major"
+# def test_powershell_get_version():
+#     command = "(get-host).version.major"
 
-    host = testinfra.get_host("powershell://")
-    result = host.run(command)
+#     host = testinfra.get_host("powershell://")
+#     result = host.run(command)
 
-    assert result.stdout.strip() == "5"
-    assert result.rc == 0
+#     assert result.stdout.strip() == "5"
+#     assert result.rc == 0
 
 
 def test_pwsh_get_version():
@@ -24,36 +24,36 @@ def test_pwsh_get_version():
     assert result.rc == 0
 
 
-def test_powershell_get_output():
-    command = "(get-host).version.major"
+# def test_powershell_get_output():
+#     command = "(get-host).version.major"
 
-    host = testinfra.get_host("powershell://")
-    result = host.check_output(command)
+#     host = testinfra.get_host("powershell://")
+#     result = host.check_output(command)
 
-    assert result == "5"
+#     assert result == "5"
 
 
 def test_ssh_file_exists():
     host = testinfra.get_host("pwsh://")
-    id_rsa_pub = host.file("~/.ssh/id_rsa.pub")
+    id_rsa_pub = host.file("test.output")
     assert id_rsa_pub.exists
 
 
 def test_ssh_file_is_file():
     host = testinfra.get_host("pwsh://")
-    id_rsa_pub = host.file("~/.ssh/id_rsa.pub")
+    id_rsa_pub = host.file("test.output")
     assert id_rsa_pub.is_file
 
 
 def test_ssh_dir_is_directory():
     host = testinfra.get_host("pwsh://")
-    id_rsa_pub = host.file("~/.ssh")
+    id_rsa_pub = host.file("testdir")
     assert id_rsa_pub.is_directory
 
 
 def test_123_file_does_not_exists():
     host = testinfra.get_host("pwsh://")
-    id_rsa_pub = host.file("~/123.123")
+    id_rsa_pub = host.file("123.123")
     assert id_rsa_pub.exists != "True"
 
 
